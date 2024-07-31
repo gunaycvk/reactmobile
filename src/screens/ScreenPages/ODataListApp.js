55e76186b15145b5eb0da067f748ef70a904d61b
gunaycvk <gunay.cvk782@icloud.com>
2024-07-31 15:26:23 +0300

    odatalist güncellendi

--- src/screens/ScreenPages/ODataListApp.js
+++ src/screens/ScreenPages/ODataListApp.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Switch, ActivityIndicator, TouchableOpacity, useColorScheme, ScrollView } from 'react-native';
-import { FlashList } from '@shopify/flash-list';
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { View, Text, StyleSheet, TouchableOpacity, ActivityIndicator, useColorScheme } from 'react-native';
 import { Searchbar } from 'react-native-paper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import { FlashList } from '@shopify/flash-list';
 import { DefaultThemeProperty } from '../../utils/appThemeStyles';
 import { useNavigation } from '@react-navigation/native';
 
@@ -33,7 +33,7 @@ const ODataListApp = () => {
   const [query, setQuery] = useState('');
   const [searchcolumns, setSearchColumns] = useState(INITIAL_SEARCH_COLUMNS);
   const [columns, setColumns] = useState(INITIAL_COLUMNS);
-  const [jwtToken, setJwtToken] = useState('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjIzNzkwMjM4LWE2NjEtNDM2Ny04NTY0LTgyNjBlZmZlN2ZmYSIsIlhhZlNlY3VyaXR5QXV0aFBhc3NlZCI6IlhhZlNlY3VyaXR5QXV0aFBhc3NlZCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJkZW5lbWV1c2VyIiwiWGFmU2VjdXJpdHkiOiJYYWZTZWN1cml0eSIsIlhhZkxvZ29uUGFyYW1zIjoicTFZS0xVNHQ4a3ZNVFZXeVVrcEp6VXZOVFMwRkNpanBLQVVrRmhlWDV4ZWxBTVVOall4TlRNMlVhZ0U9IiwiZXhwIjoxNzIyNDI4MjM1fQ.ztEBxfemQdrAdgeyHLnOFcFxqOsNhUUH-23FEkILJNo');
+  const [jwtToken, setJwtToken] = useState('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjIzNzkwMjM4LWE2NjEtNDM2Ny04NTY0LTgyNjBlZmZlN2ZmYSIsIlhhZlNlY3VyaXR5QXV0aFBhc3NlZCI6IlhhZlNlY3VyaXR5QXV0aFBhc3NlZCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJkZW5lbWV1c2VyIiwiWGFmU2VjdXJpdHkiOiJYYWZTZWN1cml0eSIsIlhhZkxvZ29uUGFyYW1zIjoicTFZS0xVNHQ4a3ZNVFZXeVVrcEp6VXZOVFMwRkNpanBLQVVrRmhlWDV4ZWxBTVVOall4TlRNMlVhZ0U9IiwiZXhwIjoxNzIyNDMyODY5fQ.tR6qU0NRBECuul4CC1ZkzDHya8dFAFyp5MqZMKcbTm0');
 
   const theme = useColorScheme();
   const isDarkMode = theme === 'dark';
@@ -68,7 +68,7 @@ const ODataListApp = () => {
 
     try {
       const response = await axios.get(
-        `http://10.10.16.10:1150/api/odata/Talepler?$select=${selectedColumns.join(',')}&$expand=ModulID($select=ModulKodu,ModulAdi),TalepTipID($select=TalepTipAdi),TalepDurumID($select=TalepDurumID,TalepDurumKodu,TalepDurumAdi,MasterDurum),TalepEdenPersonelID($select=PersonelAdi)${filterQuery ? "&$filter="+filterQuery:""}&$orderby=TalepID desc&$count=true&$skip=${page * 20}&$top=20`,
+        `http://10.10.16.10:1150/api/odata/Talepler?$select=${selectedColumns.join(',')}&$expand=ModulID($select=ModulKodu,ModulAdi),TalepTipID($select=TalepTipAdi),TalepDurumID($select=TalepDurumID,TalepDurumKodu,TalepDurumAdi,MasterDurum),TalepEdenPersonelID($select=PersonelAdi)&$orderby=TalepID desc&$count=true&$skip=${page * 20}&$top=20${filterQuery ? "&$filter=" + filterQuery : ""}`,
         {
           headers: {
             Authorization: `Bearer ${jwtToken}`
@@ -84,12 +84,12 @@ const ODataListApp = () => {
 
   const buildFilterQuery = () => {
     const filters = [];
-    if (searchcolumns.TalepID) filters.push(`TalepID eq ${query}`);
-    if (searchcolumns.TalepKonu) filters.push(`contains(TalepKonu, '${query}')`);
-    
-    
+    if (searchcolumns.TalepID && query) filters.push(`TalepID eq ${query}`);
+    if (searchcolumns.TalepKonu && query) filters.push(`contains(TalepKonu, '${query}')`);
     // Diğer kolonlar için benzer şekilde ekleme yapabilirsiniz.
-    return null//filters.join(' or ');
+    // MasterDurum'u Kapalı olanları çekmek için filtre ekleyin
+    filters.push("TalepDurumID/MasterDurum eq 'Kapali'");
+    return filters.length ? filters.join(' or ') : null;
   };
 
   const loadMore = () => {
@@ -111,38 +111,54 @@ const ODataListApp = () => {
     }
   };
 
-  const renderRequestCard = (item) => (
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('tr-TR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    }) + ' ' + date.toLocaleTimeString('tr-TR', {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+  };
 
+  const renderRequestCard = (item) => (
     <TouchableOpacity key={item.TalepID} onPress={() => navigation.navigate('TaskDetails', { task: item })}>
       <View style={[styles.card, { backgroundColor: cardBackgroundColor, borderColor: 'red' }]}>
         <Text style={[styles.projectName, { color: cardTextColor }]}>Talep Konusu: {item.TalepKonu}</Text>
-        <Text style={[styles.bodyText, { color: cardTextColor }]}>Talep No: {item.TalepID}</Text>
-        <Text style={[styles.bodyText, { color: cardTextColor }]}>Talep Edendfss: {item.TalepEdenPersonelID?.PersonelAdi}</Text>
+        <Text style={[styles.bodyText, { color: cardTextColor }]}>Talep ID: {item.TalepID}</Text>
+        <Text style={[styles.bodyText, { color: cardTextColor }]}>Talep Eden: {item.TalepEdenPersonelID?.PersonelAdi}</Text>
         <Text style={[styles.bodyText, { color: cardTextColor }]}>Talep Tipi: {item.TalepTipID?.TalepTipAdi}</Text>
+        <Text style={[styles.bodyText, { color: cardTextColor }]}>Tarih: {formatDate(item.TalepZamani)}</Text>
         <Text style={[styles.bodyText, { color: cardTextColor }]}>Durum: {item.TalepDurumID?.MasterDurum}</Text>
-        <Text style={[styles.bodyText, { color: cardTextColor }]}>Tarih: {item.TalepZamani}</Text>
-        
       </View>
     </TouchableOpacity>
   );
 
+  const renderHeader = () => (
+    <View>
+      <Searchbar
+        placeholder="Ara..."
+        onChangeText={(text) => setQuery(text)}
+        value={query}
+        onSubmitEditing={handleSearch}
+        style={styles.searchbar}
+      />
+      <Text style={[styles.headerText, { color: fontColor }]}>Kapalı Talepler</Text>
+    </View>
+  );
+
   return (
     <View style={[styles.container, { backgroundColor }]}>
-      <Searchbar
-        placeholder="Talep Ara"
-        onChangeText={setQuery}
-        value={query}
-        style={styles.searchbar}
-        inputStyle={{ color: 'black' }} 
-      />
+      {renderHeader()}
       <FlashList
         data={data}
         renderItem={({ item }) => renderRequestCard(item)}
-        keyExtractor={item => item.TalepID.toString()}
+        keyExtractor={(item) => item.TalepID.toString()}
         onEndReached={loadMore}
         onEndReachedThreshold={0.5}
-        ListFooterComponent={loading && <ActivityIndicator />}
-        estimatedItemSize={100} // Add estimated item size
+        ListFooterComponent={loading ? <ActivityIndicator size="large" color={fontColor} /> : null}
       />
     </View>
   );
@@ -151,73 +167,27 @@ const ODataListApp = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 10,
-    backgroundColor: '#000' // Added background color for better contrast
-  },
-  searchBar: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-    color: '#fff' // Changed text color to white
-  },
-  columnChooser: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    marginBottom: 10,
-  },
-  switchContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginRight: 10,
-  },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-  text: {
-    color: '#fff' // Changed text color to white
-  },
-  scrollViewContent: {
-    flexGrow: 1,
+    padding: 20,
   },
   card: {
-    marginBottom: 16,
-    padding: 16,
-    borderWidth: 1,
-    borderRadius: 8,
-    position: 'relative', // Ensure points badge is positioned relative to the card
-    overflow: 'hidden', // Ensure content doesn't overflow the card
-  },
-  projectName: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 8,
-  },
-  bodyText: {
-    fontSize: 14,
-    marginBottom: 4,
-  },
-  pointsBadge: {
-    position: 'absolute', // Position the badge absolutely within the card
-    top: 8,
-    right: 8,
-    backgroundColor: 'red',
-    color: 'white',
-    paddingHorizontal: 8,
-    paddingVertical: 4,
-    borderRadius: 12,
-  },
-  loadingIndicator: {
-    marginTop: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
   },
   searchbar: {
     marginBottom: 10,
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  projectName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  bodyText: {
+    fontSize: 14,
   },
 });
 
